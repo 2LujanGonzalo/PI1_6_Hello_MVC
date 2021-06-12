7da78ec3cffdfa3b0d02c8169fa3ef0314e35f15
gonzalo <you@example.com>
2021-06-12 20:48:52 -0300

    Hello_MVC

---
+++ controllers/index.js
@@ -0,0 +1,18 @@
+// controllers/index.js
+const User = require('../models/User');
+
+// controllers/index.js
+exports.search = (req, res) => {
+  let result = null;
+  User.findOne({ id: req.query.id }).exec((err, user) => {
+    if (user != null) {
+      user.age = Math.trunc((new Date() - user.birthday) / 31536000000);
+      result = [user];
+    }
+    res.render('index', { users: result });
+  })
+}
+
+exports.search = (req, res) => {
+  res.send('Implementar')
+}
